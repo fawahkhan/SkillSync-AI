3a409d4853dab4cc10ad3b0204672ce349b20daa
fawahkhan <khanmohdfawah21@gmail.com>
2026-03-12 00:05:52 +0530

    User login feat added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -48,3 +48,37 @@ export const registerUser = async (req,res)=>{
         return res.status(400).json({message: error.message})
     }
 }
+
+// controller for user login // POST : /api/users/login
+
+export const loginUser = async (req,res)=>{
+    try {
+        const {email, password} = req.body;
+
+        // check if user exists
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                msg: 'Invalid email or password'
+            })
+        }
+        // check if password is correct
+        if(!user.comparePassword(password)){
+            return res.status(400).json({
+                msg: 'Invalid email or password'
+            })
+        } 
+        // return success message
+
+        const token = generateToken(user._id)
+        user.password = undefined;
+
+        res.status(200).json({
+            msg: 'login successfull',
+            token,
+            user
+        })
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
